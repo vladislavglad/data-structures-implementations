f9e8c4caf4cafae3be8e28e962a45ed1a1831816
vladislavglad <vladglad.996@gmail.com>
2020-07-10 17:24:14 -0400

    Removing at an index in Linked List
    although Linked Lists are not meant to be iterated or traversed through (no random access like in an array), sometimes you need to remove elements at a certain index, which will take O(N).

--- linked-list/singly-linked.js
+++ linked-list/singly-linked.js
@@ -50,6 +50,55 @@ class LinkedList {
         return data;
     }
 
+    removeAtIndex(index) {
+
+        //check if index is at head (and tail).
+        if (index === 0)
+            return this.removeFirst();
+        
+        let previous = this.aproachToIndex(index-1);
+   
+        // Skip the Node at given index.
+        previous.next = previous.next.next;
+    }
+
+    removeByValue(data) {
+        this.removeAtIndex( this.indexOf(data) );
+    }
+
+    indexOf(data) {
+        let index = 0;
+        let node = this.head;
+        while(node !== null) {
+            if (node.data === data)
+                return index;
+            else {
+                node = node.next;
+                index++;
+            }   
+        }
+        return -1;
+    }
+
+    aproachToIndex(index) {
+
+        // Should introduce 'size' parameter 
+        // and validate index passed.
+
+        let node = this.head;
+        let currentIndex = 0;
+
+        while (node !== null && currentIndex < index) {
+            node = node.next;
+            currentIndex++;
+        }
+
+        if (node !== null && index >= 0)
+            return node;
+        else 
+            throw new RangeError("index is out of bounds!"); 
+    }
+
     toString() {
         let temp = this.head;
         let str = "";
--- linked-list/using-list.js
+++ linked-list/using-list.js
@@ -19,8 +19,31 @@ console.log("modified list: " + myList1.toString());
 console.log("removing first element: " + myList1.removeFirst());
 console.log("list after removing: " + myList1.toString());
 
-try {
-    myList3.removeFirst();
-} catch(err) {
-    console.error(err);
-}
+// // Trying to remove and from an empty list.
+// try {
+//     myList3.removeFirst();
+// } catch(err) {
+//     console.error(err);
+// }
+
+let list = new LinkedList();
+list.addLast(0);
+list.addLast(1);
+list.addLast(2);
+list.addLast(3);
+
+console.log("\nindexOf(2) " + list.indexOf(2));
+console.log("indexOf(21) " + list.indexOf(21));
+//console.log(list);
+
+//console.log(list.aproachToIndex(1));
+
+console.log("\nList before removing node at index 1:");
+console.log(list);
+list.removeAtIndex(1);
+console.log("List after removing node at index 1:");
+console.log(list);
+
+list.removeByValue(2);
+console.log("\nList after removing node by value of 2");
+console.log(list);
